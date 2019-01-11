e4f119b0931637e02b0b30e266623e993728326c
Alex Williams <Zizzs17@gmail.com>
2019-01-11 09:07:50 -0800

    Main age logic completed, allows user the input their age and then has it output to the respected part of the website.

--- src/main.js
+++ src/main.js
@@ -2,33 +2,38 @@ import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import { User } from './user';
-//import { Mercury, Venus, Mars, Jupiter, Earth } from './planets';
+import { User } from './user';
+import { Mercury, Venus, Mars, Jupiter, Earth } from './planets';
 
 
 
 
 
-$(document).ready(function (event) {
-    event.preventDefault();
+$(document).ready(function () {
+
 
     $('#userForm').submit(function (event) {
         event.preventDefault();
-        //const userName = $('#userName').val();
-        //const userAge = parseInt($('#userAge').val());
-        //let user = new User(userName, userAge);
-        //let mercury = new Mercury(.24);
-        //let venus = new Venus(.62);
-        //let earth = new Earth(1);
-        //let mars = new Mars(1.88);
-        //let jupiter = new Jupiter(11.86);
+        const userName = $('#userName').val();
+        const userAge = parseInt($('#userAge').val());
+        let user = new User(userName, userAge);
+        let mercury = new Mercury(.24);
+        let venus = new Venus(.62);
+        let earth = new Earth(1);
+        let mars = new Mars(1.88);
+        let jupiter = new Jupiter(11.86);
 
-        //const earthYear = earth.calculateEarthYears(userAge);
-        //const mercuryYear = mercury.calculateMercuryYears(userAge);
-        //const venusYear = venus.calculateVenusYears(userAge);
-        //const marsYear = mars.calculateMarsYears(userAge);
-        //const jupiterYear = jupiter.calculateJupiterYears(userAge);
+        let earthYear = earth.calculateEarthYears(user.age);
+        let mercuryYear = mercury.calculateMercuryYears(user.age, mercury.year);
+        let venusYear = venus.calculateVenusYears(user.age, venus.year);
+        let marsYear = mars.calculateMarsYears(user.age, mars.year);
+        let jupiterYear = jupiter.calculateJupiterYears(user.age, jupiter.year);
 
+        $('#earthInfo').text(`Your age in Earth years is ${earthYear} years old! ... Obviously.`);
+        $('#mercuryInfo').text(`Your age in Mercury years is ${mercuryYear} years old!`);
+        $('#venusInfo').text(`Your age in Venus years is ${venusYear} years old!`);
+        $('#marsInfo').text(`Your age in Mars years is ${marsYear} years old!`);
+        $('#jupiterInfo').text(`Your age in Jupiter years is ${jupiterYear} years old!`);
     });
 
 
